f289cea734532fdc1b11730d37560bca521b19f8
ddenis61 <ddidenko20142014@yandex.ru>
2024-07-24 08:59:38 +0300

    add_attach

--- jira.py
+++ jira.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 import json
 from base64 import b64encode
@@ -8,6 +10,7 @@ class Jira:
         self._login = login
         self._password = password
         self._base_url = base_url
+        self._token = b64encode(f'{self._login}:{self._password}'.encode('utf-8')).decode('ascii')
 
 
     def create(self, project: str, issuetype: str):
@@ -24,16 +27,39 @@ class Jira:
             }
         })
 
-        token = b64encode(f'{self._login}:{self._password}'.encode('utf-8')).decode('ascii')
 
 
         headers = {
             'Content-Type': 'application/json',
-            'Authorization': f'Basic {token}',
+            'Authorization': f'Basic {self._token}',
             'X-Atlassian-Token' : 'no-check'
         }
 
-        response = requests.request("POST", self._base_url + '/rest/api/2/issue/', headers=headers, data=payload)
+        response = requests.request("POST", self._base_url + '/rest/api/2/issue/',
+                                    headers=headers, data=payload)
         print(response.status_code)
         print(response.json())
         return response.json()['key']
+
+
+    def add_attach(self):
+        payload = {}
+        file_name = input('Введите названия файл, который нужно прикрепить с расширением(test.txt): ')
+        files = [
+            ('file',(file_name, open(file_name, 'rb'), 'text/plain'))
+        ]
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Basic {self._token}',
+            'X-Atlassian-Token': 'no-check'
+        }
+
+        key_issue = input('Введите ключ задачи к которой нужно добавить файл: ')
+
+        response = requests.request("POST", self._base_url + '/rest/api/2/issue/' + key_issue + '/attachments',
+                                    headers=headers, data=payload, files=files)
+        print(response.status_code)
+        print(response.json())
+        return  response.json()
+
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os
 from jira import Jira
 
 
-j= Jira(login='login', password='password', base_url='base_url')
-print(j.create(project='SD', issuetype='Запрос на обслуживание'))
+LOGIN = os.getenv('LOGIN')
+PASSWORD = os.getenv('PASSWORD')
+BASE_URL = os.getenv('BASE_URL')
+PROJECT = os.getenv('PROJECT')
+ISSUE_TYPE = os.getenv('ISSUE_TYPE')
+
+
+
+j= Jira(login=LOGIN, password=PASSWORD, base_url=BASE_URL)
+print(j.create(project=PROJECT, issuetype=ISSUE_TYPE))
